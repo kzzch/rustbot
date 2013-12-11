568d5d1a12af0bd62753ce400b1a2dfc11ad7d3b
Jeremiah Biard <biardjd@gmail.com>
2013-12-10 20:51:51 -0500

    Updated to Master.
    Rewrote a couple of things to take advantage of new methods from BufferedReader
    and make things a bit more succint. Also added the proper response to PING so the
    bot can at least stay indefinately connected.

--- rustdrop.rs
+++ rustdrop.rs
@@ -1,19 +1,14 @@
-use std::str;
-use std::vec;
-use std::rt::io::{Reader, Writer};
-use std::rt::io::net::tcp::TcpStream;
-use std::rt::io::net::ip::{Ipv4Addr, SocketAddr};
-use std::rt::io::buffered::BufferedStream;
+use std::io::net::tcp::TcpStream;
+use std::io::net::ip::{Ipv4Addr, SocketAddr};
+use std::io::buffered::BufferedStream;
 
 fn main() {
   let ServerAddress = Ipv4Addr(127, 0, 0, 1); // Connect to localhost
   let ServerPort = 6667;
   let SocketAddress = SocketAddr { ip: ServerAddress, port: ServerPort };
+  let mut isConnected = false;
   
-  let tcp_stream = TcpStream::connect(SocketAddress).unwrap();
-  
-  let mut buf: ~[u8] = vec::from_elem(1024, 0u8);
-  let mut stream = BufferedStream::new(tcp_stream);
+  let mut stream = BufferedStream::new(TcpStream::connect(SocketAddress).unwrap());
 
   stream.write(bytes!("NICK rustbot\r\n"));
   stream.write(bytes!("USER rustbot 8 * :rustbot\r\n"));
@@ -21,39 +16,24 @@ fn main() {
  
   let mut g = 1;
   while g > 0 {
-    let r = stream.read(buf);
-    let s = str::from_utf8_slice(buf);
+    let r = stream.read_line();
+    print(format!("{}", r));
     match r { 
-      Some(nread) => {
-        if s.contains("004")  { 
-          bot_loop()                        // This indicates we've connected to the server
-        } else {                            // successfully.
-          match s {
-            _ => {
-              ()
-              /* println(fmt!("Read %u bytes", nread));
-               * println(fmt!("%s", s));
-               */
-            }
-          }
+      Some(_) => {
+        if r.clone().unwrap().contains("004")  {
+          isConnected = true;
+        } else if r.clone().unwrap().contains("PING") {
+          println("PONG.");
+          stream.write(bytes!("PONG\r\n"));
+          stream.flush();
         }
-      },
+          
+      }
       None => {
         println!("End of Stream!");
         g = 0;
       }
     }
   }
-
   println("Connection timed out!");
 }
-
-fn bot_loop() {
-  println!("You made it, yay!");
-  loop {};
-}
-
-/*
- * ^(?:[:@]([^\\s]+) )?([^\\s]+)(?: ((?:[^:\\s][^\\s]* ?)*))?(?: ?:(.*))?$
- * regex for parsing irc messages
- */
